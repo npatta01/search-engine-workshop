2835193a2ee5f022f344b59106c681ba7844f4ba
Nidhin Pattaniyil <npatta01@gmail.com>
2022-11-10 19:03:01 -0500

    update notebooks (#3)
    Co-authored-by: Ravi <ravi@sukuya.com>
    Co-authored-by: Mustafa Zengin <mustafa.zengin@gmail.com>

--- workshop_infra/scripts/container_startup.sh
+++ workshop_infra/scripts/container_startup.sh
@@ -1,8 +1,34 @@
 
 
-gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+#gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS || echo "skipped gcloud authentication"
 
-cp -r /tmp/workshop /home/jovyan
+
+#cp -r /tmp/workshop /home/jovyan
 
 
 echo $pwd
+
+GIT_BRANCH="v2"
+GIT_BRANCH="main"
+
+echo "cloning repo"
+git clone --depth 1 https://github.com/npatta01/search-engine-workshop.git -b $GIT_BRANCH
+
+
+cd search-engine-workshop
+
+
+url="https://storage.googleapis.com/np-public-training-tmp/search-workshop/data.zip"
+
+if wget --spider $url 2>/dev/null; then
+  
+  echo "getting data from gcs"
+  wget $url 
+  unzip data.zip
+else
+  echo "getting from github"
+  wget https://github.com/npatta01/search-engine-workshop/releases/download/v1.0/data_processed.zip
+  unzip -q data_processed.zip
+
+fi
+
